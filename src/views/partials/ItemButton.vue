4344dafb4c5b380df8654629119a00a14d17cce9
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-05 00:44:37 +0100

    Update ItemButton.vue

--- src/views/partials/ItemButton.vue
+++ src/views/partials/ItemButton.vue
@@ -2,7 +2,7 @@
   <ScaleTransition>
     <button type="button" class="btn btn-card" @click="$emit('click', item)">
       <ProgressiveImage ref="image" :image="item" :alt="item.name" @preload="$emit('imagePreload')"/>
-      <div style="font-size:15pt; font-weight: bold; text-allign: center">
+      <div style="font-size:15pt; font-weight: bold; text-align: center">
         <div >
           {{ item.name }}
         </div>
