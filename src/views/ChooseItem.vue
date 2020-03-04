2956d1127838dd2bc57e65f9c1c09dcfe2671f5d
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-04 23:57:58 +0100

    Update ChooseItem.vue

--- src/views/ChooseItem.vue
+++ src/views/ChooseItem.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="app-content">
         <div class="d-block" style="width: 1550px !important">
-          <div ref="swiper" class="swiper-container">
+          <div ref="swiper" class="swiper-container" style="width: 1550px !important">
             <div class="swiper-wrapper">
               <div class="swiper-slide" v-for="item in session.itemGroup.items" :key="item.fakeId">
                 <ItemButton ref="itemButton" :item="item" @click="select(item)" @imagePreload="loadImages"/>
