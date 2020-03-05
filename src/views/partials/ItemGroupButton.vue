2c0bd274176a72f5942dff7dff858a98eab93bca
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-05 04:26:33 +0100

    Update ItemGroupButton.vue

--- src/views/partials/ItemGroupButton.vue
+++ src/views/partials/ItemGroupButton.vue
@@ -5,7 +5,7 @@
         <div ref="clipping" class="rounded-clipping">
           <ProgressiveImage ref="image" :image="itemGroup" :alt="itemGroup.name" @preload="$emit('imagePreload')"/>
         </div>
-        <div class="font-weight-bold py-3 py-lg-3">
+        <div class="font-weight-bold py-0 py-lg-0">
           {{ itemGroup.name }}
         </div>
       </button>
