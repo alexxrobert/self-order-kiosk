a078ca5b228f83953df1f8f754afff5e7888466b
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-06 17:48:58 +0100

    Update OrderItemCard.vue

--- src/views/partials/OrderItemCard.vue
+++ src/views/partials/OrderItemCard.vue
@@ -17,15 +17,15 @@
       <button type="button" class="btn btn-outline-primary btn-rounded" @click="increment">
         <FontAwesome icon="plus" class="m-auto"/>
       </button>
-      <span style="text-align: left; font-weight: bold; margin-left: 1.5rem;">{{ item.name }} {{ index }}
-      
+       <button type="button" class="btn btn-outline-primary btn-rounded"  style="margin-left: 4px" alt="Eliminar" @click="deletex">
+        <FontAwesome icon="trash" class="m-auto"/>
+      </button>
+      <span style="text-align: left; font-weight: bold; margin-left: 1.5rem;">{{ item.name }}
       </span>
       <span style="text-align: left; max-width: 930px; margin-left: 2rem;">{{ options }}</span>
       <div class="text-right font-weight-bold ml-auto">
         <Currency2 :amount="item.total()"/> 
-         <button type="button" class="btn btn-outline-primary btn-rounded"  style="padding-bottom: 4px" @click="deletex">
-        <FontAwesome icon="trash" class="m-auto"/>
-      </button>
+        
       </div>
     </div>
     
