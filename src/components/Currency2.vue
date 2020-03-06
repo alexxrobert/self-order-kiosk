b8548bea8051114e0749a222eb5c99b289fd146d
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-06 02:34:11 +0100

    Create Currency2.vue

---
+++ src/components/Currency2.vue
@@ -0,0 +1,26 @@
+  
+<template>
+  <p>
+   {{ amount | numeral(locale) }} {{ symbol }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'currency',
+  props: {
+    amount: {
+      type: Number,
+      default: () => 0
+    }
+  },
+  computed: {
+    locale() {
+      return this.$i18n.locale
+    },
+    symbol() {
+      return this.locale === 'br' ? 'R$' : '€'
+    }
+  }
+}
+</script>
