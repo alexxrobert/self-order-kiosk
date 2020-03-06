61629262dce200ba0d4aed056f7b3ceff042b460
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-06 06:56:07 +0100

    Update OrderItemCard.vue

--- src/views/partials/OrderItemCard.vue
+++ src/views/partials/OrderItemCard.vue
@@ -18,7 +18,7 @@
         <FontAwesome icon="plus" class="m-auto"/>
       </button>
       <span style="text-align: left; font-weight: bold; margin-left: 1.5rem;">{{ item.name }} {{ index }}
-       <button type="button" class="btn btn-outline-primary btn-rounded" @click="increment">
+       <button type="button" class="btn btn-outline-primary btn-rounded" @click="delete">
         <FontAwesome icon="trash-alt" class="m-auto"/>
       </button>
       </span>
@@ -50,6 +50,9 @@ export default {
     },
     increment() {
       this.session.order.increment(this.item)
+    },
+    delete(){
+    this.session.order.items.splice(index,1)
     }
   },
   computed: {
