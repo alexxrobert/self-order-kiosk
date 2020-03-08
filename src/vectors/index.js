f0d84b80082edb925a3ce1fda9b9fcb95635a5c2
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-08 16:03:24 +0100

    Update index.js

--- src/vectors/index.js
+++ src/vectors/index.js
@@ -1,9 +1,11 @@
 import VectorFlagBR from './VectorFlagBR'
 import VectorFlagUS from './VectorFlagUS'
+import VectorLogo from './VectorLogo'
 import VectorReadQRCode from './VectorReadQRCode'
 
 export {
   VectorFlagBR,
   VectorFlagUS,
-  VectorReadQRCode
+  VectorReadQRCode,
+  VectorLogo
 }
