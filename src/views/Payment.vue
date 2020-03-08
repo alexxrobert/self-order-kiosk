d91bf663a29a082eb780d8cd5628a3780c7e5152
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-08 15:09:21 +0100

    Update Payment.vue

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -13,7 +13,9 @@
          {{ $t('ammount-text') }} <Currency :amount="session.order.total()" class="text-primary font-weight-bold"/>
          </div>
           <p class="text-center mb-4">
-            <FontAwesome :icon="['far', 'smile']" size="4x"/>
+            <video src="money.mp4" autoplay loop height="300">
+  Tu navegador no admite el elemento <code>video</code>.
+</video>
           </p>
           <h3 class="text-center mb-4">
             {{ $t('thank_you') }}
