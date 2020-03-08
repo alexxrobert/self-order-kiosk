fc759a70bb7d11019075348b6f7a0123f123f599
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-03-08 13:30:07 +0100

    Create Payment.vue

---
+++ src/views/Payment.vue
@@ -0,0 +1,66 @@
+<template>
+  <SlideTransition :direction="getRouteDirection">
+    <SafeArea :class="`app theme-${session.theme}`" v-if="session.started">
+      <IdleTime :seconds="30"/>
+      <div class="app-header">
+        <div class="m-auto">
+          {{ $t('title') }}
+        </div>
+      </div>
+      <div class="app-content">
+        <div class="px-3">
+          <p class="text-center mb-4">
+            <FontAwesome :icon="['far', 'smile']" size="4x"/>
+          </p>
+          <h3 class="text-center mb-4">
+            {{ $t('thank_you') }}
+          </h3>
+          <p class="text-center">
+            {{ $t('instructions') }}
+          </p>
+        </div>
+      </div>
+      <div class="app-footer">
+        <button type="button" class="btn btn-primary mx-auto" @click="exit">
+          {{ $t('exit') }}
+        </button>
+      </div>
+    </SafeArea>
+  </SlideTransition>
+</template>
+
+<script>
+export default {
+  name: 'payment',
+  mounted() {
+    if (!this.session.started) {
+      this.restart()
+    }
+  },
+  methods: {
+    exit() {
+      this.$router.push({ name: 'newOrder' })
+    },
+    restart() {
+      this.$router.push({ name: 'start' })
+    }
+  }
+}
+</script>
+
+<i18n>
+{
+  "es": {
+    "title": "Oba! Seu pedido foi feito.",
+    "thank_you": "Obrigado!",
+    "instructions": "Te chamaremos pelo nome assim que terminarmos de preparar.",
+    "exit": "Encerrar"
+  },
+  "en": {
+    "title": "Yay! Your order has been done.",
+    "thank_you": "Thank you!",
+    "instructions": "We'll call you by name as soon as we finish cooking.",
+    "exit": "Close"
+  }
+}
+</i18n>
