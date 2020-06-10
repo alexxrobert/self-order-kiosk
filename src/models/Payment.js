1fb2a3283207c3907c47dc4eb820a88ff1b9933b
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-06-10 22:21:27 +0200

    Update Payment.js

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -2,6 +2,7 @@ export default class Payment {
   constructor(params = {}) {
     this.timer = params.timer || ''
     this.paymentstatus = params.paymentstatus || ''
+    this.opid = params.opid || 0
     this.ammount = params.ammount || ''
     this.cambio = params.cambio || ''
     this.totalpaid = params.totalpaid || ''
