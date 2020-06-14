9ff7bba48dad0727d21cc096ab1bcda79981e4a1
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-06-14 21:31:34 +0200

    Create TabacoGroupApi.js

---
+++ src/plugins/api/TabacoGroupApi.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import { ItemGroup } from '@/models';
+
+export default class TabacoGroupApi {
+  constructor() {
+    this.locale = ''
+    this.virtualPath = ''
+  }
+
+  async list() {
+    const path = `${process.env.VUE_APP_API_BASE_URL}${this.virtualPath}/menu/${this.locale}/tabaco-groups.json`
+    const response = await axios.get(path)
+
+    if (response && response.status === 200) {
+      return response.data.itemGroups.map(params => new ItemGroup(params))
+    }
+
+    return null
+  }
+}
+© 2020 GitHub, In
