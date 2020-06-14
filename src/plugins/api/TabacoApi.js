875d42bd30081ea1f3b8c9da8b25de198f97f637
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-06-14 18:07:32 +0200

    Update TabacoApi.js

--- src/plugins/api/TabacoApi.js
+++ src/plugins/api/TabacoApi.js
@@ -10,11 +10,11 @@ export default class TabacoApi {
 
   async list(TabacoId) {
     //const path = `${process.env.VUE_APP_API_BASE_URL}${this.virtualPath}/menu/${this.locale}/items/${TabacoId}.json`
-    const path = `${process.env.VUE_APP_API_BASE_URL}${this.virtualPath}/menu/${this.locale}/items/tabaco.json`
+    const path = `${process.env.VUE_APP_API_BASE_URL}${this.virtualPath}/menu/es/items/tabaco.json`
     const response = await axios.get(path)
 
     if (response && response.status === 200) {
-      return response.data.items.map(params => new Item(params))
+      return response.data.items.map(params => new Tabaco(params))
     }
 
     return null
