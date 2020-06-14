d2e5cb0a17f390a5dcb6bd8044cdfc987a35b83b
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-06-14 17:06:43 +0200

    Create Tabaco.js

---
+++ src/models/Tabaco.js
@@ -0,0 +1,41 @@
+export default class Item {
+  constructor(params = {}) {
+    this.id = params.id || ''
+    this.name = params.name || ''
+    this.price = params.price || 0
+    this.takeout = params.takeout || 'mixto'
+    this.quantity = 1
+    this.optionGroups = null
+  }
+
+  increment() {
+    if (this.quantity < 9) {
+      this.quantity++
+    }
+  }
+
+  decrement() {
+    if (this.quantity > 1) {
+      this.quantity--
+    }
+  }
+
+  options() {
+    return (this.optionGroups || [])
+      .map(optionGroup => (optionGroup.options || [])
+        .filter(option => option.checked))
+      .reduce((all, options) => all.concat(options), [])
+  }
+
+  total() {
+    let total = this.price
+
+    total += this.options()
+      .map(option => option.price)
+      .reduce((total, subtotal) => total + subtotal, 0)
+
+    total *= this.quantity
+
+    return total
+  }
+}
