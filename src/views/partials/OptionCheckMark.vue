02f9d550fc19aa71d72c8c5db441df726929c677
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-06-27 13:12:05 +0200

    Update OptionCheckMark.vue

--- src/views/partials/OptionCheckMark.vue
+++ src/views/partials/OptionCheckMark.vue
@@ -3,12 +3,12 @@
     <div ref="swiper" class="swiper-container flex-grow-1">
       <div class="swiper-wrapper">
         <label class="swiper-slide d-flex align-items-center border-bottom p-3 p-lg-5 m-0"
-          v-for="option in optionGroup.options" :key="option.id">
+          v-for="option in optionGroup.options" :key="option.id" v-if="(option.takeout =='mixto')  || ( session.order.takeOut == (option.takeout=="llevar") ) || ( !session.order.takeOut == (option.takeout=="aqui") )">
             <div class="checkbox checkbox-primary" v-if="optionGroup.multichoice">
               <input type="checkbox" v-model="option.checked" @change="toggle2($event,option)">
               <span class="check-mark"></span> {{ option.name }}
             </div>
-            <div class="radio radio-primary" v-else-if="(option.takeout =='mixto')  || ( session.order.takeOut == (option.takeout=="llevar") ) || ( !session.order.takeOut == (option.takeout=="aqui") )">
+            <div class="radio radio-primary" v-else>
               <input type="checkbox" v-model="option.checked" @change="toggle($event, option)">
               <span class="check-mark"></span> {{ option.name }}
             </div>
