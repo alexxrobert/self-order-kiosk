e422b72952b6669b4a2a15b3824dc14606489fab
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-06-30 19:59:35 +0200

    Update PaymentTabaco.vue

--- src/views/PaymentTabaco.vue
+++ src/views/PaymentTabaco.vue
@@ -116,7 +116,8 @@ export default {
     if (this.payment.paymentstatus === 'finished'){
         clearInterval(this.polling)
 	//setTimeout(()=>{ this.exit(); }, 10000)
-	setTimeout(()=>{ finishorder(); }, 5000)
+	//setTimeout(()=>{ finishorder(); }, 5000)
+	finishorder()
     	return true
 	}
     console.log('pstatus= '+ this.payment.paymentstatus+' amm=' + this.payment.ammount + this.payment.opid)
