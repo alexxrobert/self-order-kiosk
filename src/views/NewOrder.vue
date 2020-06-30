ddefa4f85e538a61a6b773040c033d400022903f
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-06-30 23:17:55 +0200

    Update NewOrder.vue

--- src/views/NewOrder.vue
+++ src/views/NewOrder.vue
@@ -10,7 +10,7 @@
         </h1>
       </div>
       <div class="app-content">
-     <div id="activetabaco" v-bind:style="newOrder.activated ? 'opacity: 1;': 'opacity: 0.30'  style="overflow: hidden; opacity: 0.30;">
+     <div id="activetabaco" v-bind:style="newOrder.activated ? 'opacity: 1;': 'opacity: 0.30'"  style="overflow: hidden; opacity: 0.30;">
       <img src="/img/tabaco18.jpg" style="position: absolute; top: 190px; left: 20px;" @click="newTabaco">
        <p v-if="isTabacoactivated"></p>
        <p style="font-size: 1.5rem; position: absolute; top: 480px; left: 10px;" v-else>Comprar tabaco, preguntar en la barra</p>
