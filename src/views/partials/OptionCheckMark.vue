201b876fe76f180a1d3bc278848d25d44cc101a7
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-07-11 20:20:46 +0200

    Update OptionCheckMark.vue

--- src/views/partials/OptionCheckMark.vue
+++ src/views/partials/OptionCheckMark.vue
@@ -100,7 +100,7 @@ export default {
     toggley($event,current){
       if(current.more==true){
        current.more=false
-      }elseif(current.checked=false){
+      }elseif(current.checked==false){
           current.more=true
      }
     },
