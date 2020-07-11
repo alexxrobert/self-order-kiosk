34d972d95f503a87fde396c2189a9e7acdddf42c
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2020-07-11 13:50:04 +0200

    Update OptionCheckMark.vue

--- src/views/partials/OptionCheckMark.vue
+++ src/views/partials/OptionCheckMark.vue
@@ -5,17 +5,17 @@
         <div class="swiper-slide d-flex align-items-center border-bottom p-3 p-lg-5 m-0"
           v-for="option in optionGroup.options" :key="option.id" v-if="(!option.outofstock) && ( (option.takeout =='mixto')  || ( session.order.takeOut == (option.takeout=='llevar') ) || ( !session.order.takeOut == (option.takeout=='aqui') ) )">
               <div class="checkbox checkbox-primary" v-if="optionGroup.multichoice">
-                <input type="checkbox" v-model="option.checked" @change="toggle2($event,option)">
-                <label class="check-mark"></label> {{ option.name }}
+                <input type="checkbox"  v-bind:id="option.id" v-model="option.checked" @change="toggle2($event,option)">
+                <label v-bind:for="option.id" class="check-mark"></label> {{ option.name }}
                 <img v-bind:id="'img' +option.id" v-bind:style="option.checked ? 'opacity: 1;': 'opacity: 0.20' " style="position: absolute; left:300px;  box-sizing:border-box;"  v-bind:src="imageUrl + option.id +'.jpg' " />
               </div>
               <div style="position: absolute; left: 480px" class="checkbox2 checkbox-primary" v-if="optionGroup.multichoice && option.modifier == false ">
-                <input type="checkbox" id="more" name="" value="true" v-model="option.more" >
-                <label class="check-mark" id="more2"></label>Mas {{ option.name }}
+                <input type="checkbox" v-bind:id="option.id" v-model="option.more" >
+                <label v-bind:for="option.id" class="check-mark" id="more2"></label>Mas {{ option.name }}
                </div>
               <div class="radio radio-primary" v-else-if="optionGroup.multichoice == false">
-                <input type="checkbox" v-model="option.checked" @change="toggle($event, option)">
-                <label class="check-mark"></label> {{ option.name }}
+                <input type="checkbox" v-bind:id="option.id" v-model="option.checked" @change="toggle($event, option)">
+                <label v-bind:for="option.id" class="check-mark"></label> {{ option.name }}
               </div>
               <div class="ml-auto text-primary" v-if="option.price > 0">
                 <Currency :amount="option.price"/>
